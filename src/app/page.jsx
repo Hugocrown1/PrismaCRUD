3c4c0caf52858c3d4655c999ae8d2b367f00d5a5
Hugo Corona <hugo.corona.r@gmail.com>
2023-10-24 13:37:32 -0700

    barra de busqueda y spinner de carga

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,15 +11,22 @@ export default function Home() {
   const [showModalEdit, setShowModalEdit] = useState(false);
   const [showModalDelete, setShowModalDelete] = useState(false);
 
+  const [loadingContent, setLoadingContent] = useState(false);
+
   const [editedStudent, setEditedStudent] = useState({});
   const [deletedStudent, setDeletedStudent] = useState({});
 
+  const [filter, setFilter] = useState("");
+  const [studentsList, setStudentsList] = useState([]);
+
   const fetchStudents = () => {
+    setLoadingContent(true);
     fetch("/api/registros")
       .then((response) => {
         if (!response.ok) {
           throw new Error("La solicitud no fue exitosa");
         }
+
         return response.json();
       })
       .then((data) => {
@@ -27,12 +34,38 @@ export default function Home() {
       })
       .catch((error) => {
         console.error("Error en la solicitud:", error);
+      })
+      .finally(() => {
+        setLoadingContent(false);
       });
   };
 
   useEffect(() => {
     fetchStudents();
   }, []);
+
+  const filterStudents = () => {
+    const filters = filter.toLowerCase().split(" ");
+
+    return students.filter(({ id, nombre, genero, edad, carrera }) => {
+      const attributes = [
+        id.toString(),
+        nombre,
+        genero,
+        edad.toString(),
+        carrera,
+      ]
+        .join(" ")
+        .toLowerCase();
+      return filters.every((filterValue) => attributes.includes(filterValue));
+    });
+  };
+
+  useEffect(() => {
+    const filteredStudents = filterStudents();
+
+    setStudentsList(filteredStudents);
+  }, [filter, students]);
 
   const saveStudent = async (student) => {
     if (!student.id) {
@@ -76,14 +109,30 @@ export default function Home() {
 
   return (
     <main className=" flex justify-center mx-auto max-w-7xl  p-5 ">
-      <section className="w-[80%] mt-8">
-        <button
-          className="flex items-center justify-center bg-[#9d4edd] w-full px-12 py-3 text-white font-semibold text-lg rounded-lg mb-4"
-          onClick={() => setShowModal(true)}
-        >
-          <span className="mr-2">Agregar nuevo estudiante</span>
-          <IconPlus />
-        </button>
+      <section className="inset-0 relative w-[80%] mt-2">
+        <h1 className="text-center font-bold text-5xl my-6">Prisma CRUD</h1>
+        <div className="flex items-center justify-between">
+          <div>
+            <label className="mb-2 font-semibold" htmlFor="search">
+              Buscar:{" "}
+            </label>
+            <input
+              className="ring-black ring-1 mb-2"
+              id="search"
+              placeholder="Nombre, genero, edad, ..."
+              type="text"
+              value={filter}
+              onChange={(e) => setFilter(e.target.value)}
+            />
+          </div>
+          <button
+            className="flex items-center justify-center bg-[#9d4edd] w-fit min-w-[100px] px-6 py-3 text-white font-semibold text-base rounded-lg mb-2"
+            onClick={() => setShowModal(true)}
+          >
+            <span className="mr-2">Agregar nuevo estudiante</span>
+            <IconPlus />
+          </button>
+        </div>
         <table className="w-full border-collapse bg-white table-auto rounded-md overflow-hidden ">
           <thead className="bg-[#e5e5e5]">
             <tr className="font-semibold">
@@ -95,8 +144,9 @@ export default function Home() {
               <td>Acciones</td>
             </tr>
           </thead>
+
           <tbody>
-            {students.map((student) => (
+            {studentsList.map((student) => (
               <tr className="border-y border-slate-600/10" key={student.id}>
                 <td>{student.id}</td>
                 <td>{student.nombre}</td>
@@ -124,6 +174,15 @@ export default function Home() {
             ))}
           </tbody>
         </table>
+
+        {loadingContent && (
+          <svg
+            className=" absolute w-[10%] translate-x-[440px] translate-y-28"
+            viewBox="25 25 50 50"
+          >
+            <circle r="20" cy="50" cx="50"></circle>
+          </svg>
+        )}
 
         <Modal isVisible={showModal}>
           <h2 className="text-2xl font-bold text-center my-4">
