f7b3191cc1981bc662c14813012a1d0b20d46dfa
Hugo Corona <hugo.corona.r@gmail.com>
2023-10-24 01:51:42 -0700

    primera version del CRUD

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,21 @@
-'use client'
-import Image from 'next/image'
-import { useEffect, useState } from 'react'
+"use client";
+import Modal from "@/components/Modal";
+import StudentForm from "@/components/StudentForm";
+import { IconEdit, IconPlus, IconTrash } from "@tabler/icons-react";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [records, setRecords] = useState([])
-  
+  const [students, setStudents] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [showModalEdit, setShowModalEdit] = useState(false);
+  const [showModalDelete, setShowModalDelete] = useState(false);
 
-  useEffect(() => {
-    fetch("/api/registros", {})
+  const [editedStudent, setEditedStudent] = useState({});
+  const [deletedStudent, setDeletedStudent] = useState({});
+
+  const fetchStudents = () => {
+    fetch("/api/registros")
       .then((response) => {
         if (!response.ok) {
           throw new Error("La solicitud no fue exitosa");
@@ -15,38 +23,157 @@ export default function Home() {
         return response.json();
       })
       .then((data) => {
-        setRecords(data);
+        setStudents(data);
       })
       .catch((error) => {
         console.error("Error en la solicitud:", error);
       });
-  
-    
-  }, [])
-  
-  return (
-   <main className=" flex justify-center mx-auto max-w-7xl  p-5 ">
-    <table className='w-[70%] h-fit bg-white rounded-md'>
-      <thead className='bg-slate-600 p-12'>
-        <tr className='p-12'>
-          <td>ID</td>
-          <td>Nombre</td>
-          <td>Genero</td>
-          <td>Edad</td>
-          <td>Carrera</td>
-        </tr>
-      </thead>
-      <tbody>
-        {records.map(student => (<tr key={student.id}>
-          <td>{student.id}</td>
-          <td>{student.nombre}</td>
-          <td>{student.genero}</td>
-          <td>{student.edad}</td>
-          <td>{student.carrera}</td>
+  };
 
-        </tr>))}
-      </tbody>
-    </table>
-   </main>
-  )
+  useEffect(() => {
+    fetchStudents();
+  }, []);
+
+  const saveStudent = async (student) => {
+    if (!student.id) {
+      await fetch("/api/registros", {
+        method: "POST",
+        body: JSON.stringify(student),
+        headers: {
+          "Content-type": "application/json",
+        },
+      });
+    } else {
+      await fetch("/api/registros/" + student.id, {
+        method: "PUT",
+        body: JSON.stringify(student),
+        headers: {
+          "Content-type": "application/json",
+        },
+      });
+      setEditedStudent({});
+    }
+
+    fetchStudents();
+  };
+
+  const editStudent = (student) => {
+    setEditedStudent(student);
+    setShowModalEdit(true);
+  };
+
+  const deleteStudent = async () => {
+    await fetch("/api/registros/" + deletedStudent.id, {
+      method: "DELETE",
+
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+    setDeletedStudent({});
+    fetchStudents();
+  };
+
+  return (
+    <main className=" flex justify-center mx-auto max-w-7xl  p-5 ">
+      <section className="w-[80%] mt-8">
+        <button
+          className="flex items-center justify-center bg-[#9d4edd] w-full px-12 py-3 text-white font-semibold text-lg rounded-lg mb-4"
+          onClick={() => setShowModal(true)}
+        >
+          <span className="mr-2">Agregar nuevo estudiante</span>
+          <IconPlus />
+        </button>
+        <table className="w-full border-collapse bg-white table-auto rounded-md overflow-hidden ">
+          <thead className="bg-[#e5e5e5]">
+            <tr className="font-semibold">
+              <td className="py-2">ID</td>
+              <td>Nombre</td>
+              <td>Genero</td>
+              <td>Edad</td>
+              <td>Carrera</td>
+              <td>Acciones</td>
+            </tr>
+          </thead>
+          <tbody>
+            {students.map((student) => (
+              <tr className="border-y border-slate-600/10" key={student.id}>
+                <td>{student.id}</td>
+                <td>{student.nombre}</td>
+                <td>{student.genero}</td>
+                <td>{student.edad}</td>
+                <td>{student.carrera}</td>
+                <td className="flex">
+                  <button
+                    className="bg-[#9d4edd] table-button"
+                    onClick={() => editStudent(student)}
+                  >
+                    <IconEdit /> Editar
+                  </button>
+                  <button
+                    className="bg-[#ff6d00] table-button"
+                    onClick={() => {
+                      setDeletedStudent(student);
+                      setShowModalDelete(true);
+                    }}
+                  >
+                    <IconTrash /> Eliminar
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        <Modal isVisible={showModal}>
+          <h2 className="text-2xl font-bold text-center my-4">
+            Nuevo estudiante
+          </h2>
+          <StudentForm
+            saveStudent={saveStudent}
+            onClose={() => setShowModal(false)}
+          />
+        </Modal>
+
+        <Modal isVisible={showModalEdit}>
+          <h2 className="text-2xl font-bold text-center my-4">
+            Editar estudiante
+          </h2>
+          <StudentForm
+            saveStudent={saveStudent}
+            onClose={() => setShowModalEdit(false)}
+            {...editedStudent}
+          />
+        </Modal>
+
+        <Modal isVisible={showModalDelete}>
+          <h2 className="text-2xl font-bold text-center my-4">
+            Eliminar a {deletedStudent.nombre}?
+          </h2>
+          <div className="flex space-x-6">
+            <button
+              type="button"
+              className="bg-[#9d4edd] px-4 py-2 text-white font-semibold rounded-md w-full"
+              onClick={() => {
+                deleteStudent();
+                setShowModalDelete(false);
+              }}
+            >
+              Eliminar
+            </button>
+            <button
+              type="button"
+              className="bg-[#ff6d00] px-4 py-2 text-white font-semibold rounded-md w-full"
+              onClick={() => {
+                setDeletedStudent({});
+                setShowModalDelete(false);
+              }}
+            >
+              Cancelar
+            </button>
+          </div>
+        </Modal>
+      </section>
+    </main>
+  );
 }
---
+++ src/components/StudentForm.jsx
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+
+const StudentForm = ({
+  saveStudent,
+  onClose,
+  id,
+  nombre: existingName,
+  genero: existingGender,
+  edad: existingAge,
+  carrera: existingCareer,
+}) => {
+  const [name, setName] = useState(existingName || "");
+  const [gender, setGender] = useState(existingGender || "");
+  const [age, setAge] = useState(existingAge || "");
+  const [career, setCareer] = useState(existingCareer || "");
+
+  const handleCancel = (e) => {
+    e.preventDefault();
+    onClose();
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const student = {
+      id,
+      nombre: name,
+      genero: gender,
+      edad: parseFloat(age),
+      carrera: career,
+    };
+
+    saveStudent(student);
+    onClose();
+  };
+  return (
+    <form
+      onSubmit={handleSubmit}
+      onReset={handleCancel}
+      className="flex flex-col mx-auto max-w-lg"
+    >
+      <input
+        required
+        placeholder="Nombre"
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <select
+        required
+        className={`${gender === "" ? "text-[#9CA3AF]" : "text-black"}`}
+        value={gender}
+        onChange={(e) => setGender(e.target.value)}
+      >
+        <option className="text-gray-500" value="" disabled>
+          Genero
+        </option>
+
+        <option className="text-black" value="Masculino">
+          Masculino
+        </option>
+        <option className="text-black" value="Femenino">
+          Femenino
+        </option>
+      </select>
+      <input
+        required
+        placeholder="Edad"
+        type="number"
+        value={age}
+        onChange={(e) => {
+          setAge(e.target.value);
+        }}
+      />
+      <input
+        required
+        placeholder="Carrera"
+        type="text"
+        value={career}
+        onChange={(e) => setCareer(e.target.value)}
+      />
+      <div className="flex justify-center space-x-4 mb-4">
+        <button
+          type="submit"
+          className="bg-[#9d4edd] px-4 py-2 text-white font-semibold rounded-md w-full"
+        >
+          Enviar
+        </button>
+        <button
+          type="reset"
+          className="bg-[#ff6d00] px-4 py-2 text-white font-semibold rounded-md w-full"
+        >
+          Cancelar
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default StudentForm;
